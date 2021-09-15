0885aadfae3ccb7b0375178154bd5f9f4722bf85
Ubuntu <ubuntu@ip-172-31-5-207.us-east-2.compute.internal>
2021-09-15 01:49:28 +0000

    Updated Out.txt file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,13 +8,13 @@ dockerpath=rajpurp/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run prakashproject --image=$dockerpath --port=80 --labels app=prakashproject
+minikube kubectl run prakashproject --image=$dockerpath --port=80 --labels app=prakashproject
 
 
 # Step 3:
 # List kubernetes pods
-kubectl get pod
+minikube kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward prakashproject 8000:80
+minikube kubectl port-forward prakashproject 8000:80
